690ca16ef5c893de52a052f07c5964c4dad244a6
mad201802 <michael.dick71@googlemail.com>
2021-01-27 18:45:47 +0100

    Add edit fridge endpoint, add handler and validator

---
+++ Application/Fridges/Commands/Edit/EditFridgeCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using MediatR;
+using MyFridgeListWebapi.Core.Models.Responses.Fridge;
+
+namespace MyFridgeListWebapi.Application.Fridges.Commands.Edit
+{
+    public sealed class EditFridgeCommand : IRequest<EditFridgeResponse>
+    {
+        [JsonIgnore]
+        public Guid UserId { get; set; }
+        [JsonIgnore]
+        public Guid FridgeId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Application/Fridges/Commands/Edit/EditFridgeCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using FluentValidation;
+using MyFridgeListWebapi.Core.Data.Database;
+using MyFridgeListWebapi.Properties;
+
+namespace MyFridgeListWebapi.Application.Fridges.Commands.Edit
+{
+    public sealed class EditFridgeCommandValidator : AbstractValidator<EditFridgeCommand>
+    {
+        public EditFridgeCommandValidator(DatabaseContext dbContext)
+        {
+            RuleFor(request => request.FridgeId)
+                .NotEmpty()
+                .WithMessage(Resources.ValidationErrorMissingFridgeId);
+
+            RuleFor(request => request.FridgeId)
+                .Must(id => dbContext.Fridges.Any(fridge => fridge.Id == id))
+                .WithMessage(request => string.Format(Resources.ValidationErrorFridgeWithIdNotExists, request.FridgeId));
+        }
+    }
+}
