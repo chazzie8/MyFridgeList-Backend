2d62779da1b9de36840581282b1109f167d60282
mad201802 <michael.dick71@googlemail.com>
2021-02-02 21:37:12 +0100

    add article get validator

---
+++ Application/Articles/Queries/All/GetArticleQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MyFridgeListWebapi.Core.Data.Database;
+using MyFridgeListWebapi.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFridgeListWebapi.Application.Articles.Queries.All
+{
+    public sealed class GetArticleQueryValidator : AbstractValidator<GetArticlesQuery>
+    {
+        public GetArticleQueryValidator(DatabaseContext dbContext)
+        {
+            RuleFor(request => request.FridgeId)
+                .Must(id => dbContext.Fridges.Any(fridge => fridge.Id == id))
+                .WithMessage(request => string.Format(Resources.ValidationErrorFridgeWithIdNotExists, request.FridgeId));
+        }
+    }
+}
