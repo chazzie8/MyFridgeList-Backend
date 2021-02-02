5aae7ee9583f0a75de71f4c806aaa11ed63479ee
Chas Richards <chas.richards@seracom.de>
2021-02-02 21:50:44 +0100

    add DeleteItemCommand

---
+++ Application/Items/Commands/Delete/DeleteItemCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using MediatR;
+using MyFridgeListWebapi.Core.Models.Responses.Item;
+
+namespace MyFridgeListWebapi.Application.Items.Commands.Delete
+{
+    public sealed class DeleteItemCommand : IRequest<DeleteItemResponse>
+    {
+        [JsonIgnore]
+        public Guid ShoppinglistId { get; set; }
+        [JsonIgnore]
+        public Guid ItemId { get; set; }
+    }
+}
