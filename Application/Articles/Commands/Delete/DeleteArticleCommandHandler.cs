876307f4acaf03b10b1b827203a78a2079fa4e17
mad201802 <michael.dick71@googlemail.com>
2021-02-02 20:32:28 +0100

    add delete article handler

---
+++ Application/Articles/Commands/Delete/DeleteArticleCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MyFridgeListWebapi.Core.Data.Database;
+using MyFridgeListWebapi.Core.Models.Responses.Article;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyFridgeListWebapi.Application.Articles.Commands.Delete
+{
+    public sealed class DeleteArticleCommandHandler : IRequestHandler<DeleteArticleCommand, DeleteArticleResponse>
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public DeleteArticleCommandHandler(DatabaseContext databaseContext)
+        {
+            _dbContext = databaseContext;
+        }
+
+        public async Task<DeleteArticleResponse> Handle(DeleteArticleCommand request, CancellationToken cancellationToken)
+        {
+            var article = await _dbContext.Articles
+                .Where(x => x.Id == request.ArticleId)
+                .FirstOrDefaultAsync();
+
+            _dbContext.Articles.Remove(article);
+            await _dbContext.SaveChangesAsync();
+
+            return new DeleteArticleResponse();
+        }
+    }
+}
