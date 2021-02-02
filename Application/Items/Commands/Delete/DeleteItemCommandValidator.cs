23f5ec0bce38ae6c385a690e3dcfb61a280e8d16
Chas Richards <chas.richards@seracom.de>
2021-02-02 21:58:59 +0100

    add DeleteItemCommandValidator

---
+++ Application/Items/Commands/Delete/DeleteItemCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using FluentValidation;
+using MyFridgeListWebapi.Core.Data.Database;
+using MyFridgeListWebapi.Properties;
+
+namespace MyFridgeListWebapi.Application.Items.Commands.Delete
+{
+    public class DeleteItemCommandValidator : AbstractValidator<DeleteItemCommand>
+    {
+        public DeleteItemCommandValidator(DatabaseContext dbContext)
+        {
+            RuleFor(request => request.ShoppinglistId)
+                .Must(id => dbContext.Fridges.Any(shoppinglist => shoppinglist.Id == id))
+                .WithMessage(request => string.Format(Resources.ValidationErrorShoppinglistWithIdNotExists, request.ShoppinglistId));
+
+            RuleFor(request => request.ItemId)
+                .Must(id => dbContext.Articles.Any(item => item.Id == id))
+                .WithMessage(request => string.Format(Resources.ValidationErrorItemWithIdNotExists, request.ItemId));
+        }
+    }
+}
