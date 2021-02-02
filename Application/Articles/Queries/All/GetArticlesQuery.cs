ef7d72ccc5cb01f5cff8701248830f0c48957b0d
mad201802 <michael.dick71@googlemail.com>
2021-02-02 21:36:39 +0100

    add article get query and response model

---
+++ Application/Articles/Queries/All/GetArticlesQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using MyFridgeListWebapi.Core.Models.Responses.Article;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFridgeListWebapi.Application.Articles.Queries.All
+{
+    public sealed class GetArticlesQuery : IRequest<IEnumerable<ArticleResponse>>
+    {
+        public Guid FridgeId { get; set; }
+    }
+}
