8d812fdca9438ed8961bd8c167dc8e74fefcb7e9
Chas Richards <chas.richards@seracom.de>
2021-02-03 10:55:21 +0100

    add GetItemsQueryHandler

---
+++ Application/Items/Queries/All/GetItemsQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MyFridgeListWebapi.Core.Data.Database;
+using MyFridgeListWebapi.Core.Models.Responses.Item;
+
+namespace MyFridgeListWebapi.Application.Items.Queries.All
+{
+    public sealed class GetItemsQueryHandler : IRequestHandler<GetItemsQuery, IEnumerable<ItemResponse>>
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public GetItemsQueryHandler(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+        public async Task<IEnumerable<ItemResponse>> Handle(GetItemsQuery request, CancellationToken cancellationToken)
+        {
+            var items = await _databaseContext.Items
+                .Where(x => x.ShoppinglistId == request.ShoppinglistId)
+                .Select(x => new ItemResponse
+                {
+                    Id = x.Id,
+                    Label = x.Label,
+                    Bought = x.Bought
+                })
+                .ToListAsync(cancellationToken);
+
+            return items;
+        }
+    }
+}
