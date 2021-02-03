bf63d8251674a16876588971e499382d4cff80f8
Chas Richards <chas.richards@seracom.de>
2021-02-03 09:54:51 +0100

    add ItemResponse and GetItemsQuery

---
+++ Application/Items/Queries/All/GetItemsQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using MyFridgeListWebapi.Core.Models.Responses.Item;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFridgeListWebapi.Application.Items.Queries.All
+{
+    public sealed class GetItemsQuery : IRequest<IEnumerable<ItemResponse>>
+    {
+        public Guid ShoppinglistId { get; set; }
+    }
+}
